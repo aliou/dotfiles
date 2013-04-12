a3d33cc2eac80d4af8c965e75f5d8870f068edd7
Aliou Diallo <aliou.diallo@me.com>
2013-04-13 01:54:01 +0200

    [SYSTEM] Use value instead of variable for text color reset.

--- system/prompt.sh
+++ system/prompt.sh
@@ -10,7 +10,7 @@ _PS1folder="$txtwht \w"
 _PS1git=" $bldblu\$(parse_git_branch)"
 _PS1svn="$bldgrn\$(parse_svn_revision)"
 _PS1JOBS="\n$bldred\${hasjobs:+[\j] }"
-_PS1end="$txtrst› "
+_PS1end="\e[0m› "
 
 export PS1="$_PS1host$_PS1folder$_PS1git$_PS1svn$_PS1JOBS$_PS1end"
 
