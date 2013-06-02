aedcd46a935f23c5c024f4e84b2a985de6d4e4c6
Aliou Diallo <diallo_e@epitech.net>
2013-06-02 20:30:36 +0200

    [HG] Show current branch and state in prompt.

---
+++ hg/prompt.sh
@@ -0,0 +1,17 @@
+function hg_in_repo() {
+  hg branch 2> /dev/null | awk '{print "on "}'
+}
+
+function hg_dirty() {
+  hg status --no-color 2> /dev/null \
+    | awk '$1 == "?" { print "?" } $1 != "?" { print "!" }' \
+    | sort | uniq | head -c1
+}
+
+function hg_branch() {
+  hg branch 2> /dev/null | awk '{print $1}'
+}
+
+function hg_prompt() {
+  echo "($(hg_branch)$(hg_dirty))"
+}
