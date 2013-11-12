24c72440d41c16d72bae00e74361d13efe15f524
Aliou Diallo <code@aliou.me>
2013-11-12 21:43:18 +0100

    Update install script.

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@
 ln -Fs ~/.dotfiles/bash/bashrc ~/.bashrc
 ln -Fs ~/.dotfiles/bash/inputrc ~/.inputrc
 ln -Fs ~/.dotfiles/bash/profile ~/.profile
+ln -Fs ~/.dotfiles/tcsh/tcshrc ~/.tcshrc
 
 ln -Fs ~/.dotfiles/git/gitignore ~/.gitignore
 ln -Fs ~/.dotfiles/git/gitconfig ~/.gitconfig
