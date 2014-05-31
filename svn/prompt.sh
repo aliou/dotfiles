f4f9b650966fbad705212318f1ebe0a3bef75b05
Aliou Diallo <code@aliou.me>
2014-05-31 23:24:31 +0200

    Fix indentation.

--- svn/prompt.sh
+++ svn/prompt.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 parse_svn_revision() {
-	local DIRTY REV=$(svn info 2>/dev/null | grep Revision | sed -e 's/Revision: //')
-	[ "$REV" ] || return
-	[ "$(svn st)" ] && DIRTY='#'
-	echo "{rev :$REV$DIRTY}"
+  local DIRTY REV=$(svn info 2>/dev/null | grep Revision | sed -e 's/Revision: //')
+  [ "$REV" ] || return
+  [ "$(svn st)" ] && DIRTY='#'
+  echo "{rev :$REV$DIRTY}"
 }
