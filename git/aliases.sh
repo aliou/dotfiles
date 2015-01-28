b285e5a05d49702c9c17110dc0a11e4b52824cf9
Aliou Diallo <code@aliou.me>
2015-01-28 22:17:59 +0100

    Probably a bad idea.
    If there's any problem with git, this is the commit to revert.

--- git/aliases.sh
+++ git/aliases.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
 # HERP DERP
-alias g=git
-alias gt=git
-alias gti=git
-alias qgit=git
+# alias gt=git
+# alias gti=git
+# alias qgit=git
+
+# Train my muscle memory to use the `g` alias:
+# 1. Alias `g` to the binary instead of the command `git` to avoid recursive
+# bullshit.
+alias g=`which git`
+
+# 2. Alias `git` to the no-op command so I lose my mind.
+# (99% sure this is a bad idea).
+alias git=:
+
+# 3. Add completion <TAB> completion to `g`.
+# Instructions in `/usr/local/etc/bash_completion.d/git-completion.bash`.
+__git_complete g _git
+
+# 4. Do the same thing for `gc`, `gco`, `gps`, `gpl` etc.
+alias gup=git-up
