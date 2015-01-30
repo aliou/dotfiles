b986d9c39d03ecc756bafdc51e8c68d7a67f26fc
Aliou Diallo <code@aliou.me>
2015-01-30 13:09:38 +0100

    Update git prompt functions.

--- git/prompt.sh
+++ git/prompt.sh
@@ -2,21 +2,21 @@
 # https://gist.github.com/48207
 
 function parse_git_deleted {
-    [[ $(git status 2> /dev/null | grep deleted:) != "" ]] && echo "-"
+    [[ $(`which git` status 2> /dev/null | grep deleted:) != "" ]] && echo "-"
 }
 function parse_git_added {
-    [[ $(git status 2> /dev/null | grep "Untracked files:") != "" ]] && echo '+'
+    [[ $(`which git` status 2> /dev/null | grep "Untracked files:") != "" ]] && echo '+'
 }
 function parse_git_modified {
-    [[ $(git status 2> /dev/null | grep modified:) != "" ]] && echo "#"
+    [[ $(`which git` status 2> /dev/null | grep modified:) != "" ]] && echo "#"
 }
 function parse_git_stashed {
-    stashed=$(git stash list 2> /dev/null | wc -l | sed 's/ //g')
+    stashed=$(`which git` stash list 2> /dev/null | wc -l | sed 's/ //g')
     [[ $stashed -ne 0 ]] && echo "[$stashed]"
 }
 function parse_git_dirty {
     echo "$(parse_git_added)$(parse_git_modified)$(parse_git_deleted)"
 }
 function parse_git_branch {
-    git branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]$(parse_git_stashed) /"
+    `which git` branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]$(parse_git_stashed) /"
 }
