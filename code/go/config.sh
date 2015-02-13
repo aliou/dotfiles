07943724438cc73f4e81ea3fa267c9158f4d8fab
Aliou Diallo <code@aliou.me>
2015-02-13 23:14:23 +0100

    Test if go is installed before setting env vars.

--- code/go/config.sh
+++ code/go/config.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-export GOPATH="$HOME/src/.go"
-export GOBIN="$GOPATH/bin"
-export PATH="$PATH:$GOBIN"
+if [[ -n `which go` ]]; then
+  export GOPATH="$HOME/src/.go"
+  export GOBIN="$GOPATH/bin"
+  export PATH="$PATH:$GOBIN"
+fi
