66f22b6b9fbe4e98f70bfffe2736540d11a344fb
Aliou Diallo <code@aliou.me>
2015-02-13 14:52:06 +0100

    Add some hadoop env vars.

--- code/hadoop/config.sh
+++ code/hadoop/config.sh
@@ -3,4 +3,9 @@
 if [[ -n `which hadoop` ]]; then
   export HADOOP_HOME="/usr/local/Cellar/hadoop/2.6.0"
   export HADOOP_CONF_DIR="$HADOOP_HOME/libexec/etc/hadoop"
+
+  export JAVA_HOME=$(/usr/libexec/java_home)
+
+  export HIVE_HOME="/usr/local/Cellar/hive/0.14.0/libexec"
+  export HCAT_HOME="/usr/local/Cellar/hive/0.14.0/libexec/hcatalog"
 fi
