7cb8612b33e76fbddbbe02975db357e5bbaaf035
Aliou Diallo <code@aliou.me>
2015-02-13 09:50:39 +0100

    Don't wrap exports in function.

--- code/hadoop/config.sh
+++ code/hadoop/config.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
 
-hadoop_setup() {
-  export HADOOP_HOME="/usr/local/Cellar/hadoop/2.6.0"
-  export HADOOP_CONF_DIR="$HADOOP_HOME/libexec/etc/hadoop"
-}
+export HADOOP_HOME="/usr/local/Cellar/hadoop/2.6.0"
+export HADOOP_CONF_DIR="$HADOOP_HOME/libexec/etc/hadoop"
