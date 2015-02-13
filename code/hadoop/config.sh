cdc6bc105e65abae15be061b84014277fe7ad532
Aliou Diallo <code@aliou.me>
2015-02-13 14:51:39 +0100

    Only export hadoop envvars if it is installed.

--- code/hadoop/config.sh
+++ code/hadoop/config.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
 
-export HADOOP_HOME="/usr/local/Cellar/hadoop/2.6.0"
-export HADOOP_CONF_DIR="$HADOOP_HOME/libexec/etc/hadoop"
+if [[ -n `which hadoop` ]]; then
+  export HADOOP_HOME="/usr/local/Cellar/hadoop/2.6.0"
+  export HADOOP_CONF_DIR="$HADOOP_HOME/libexec/etc/hadoop"
+fi
