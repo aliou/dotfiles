243b1b33bf6f40091c22e98fd5ea1245ed43fa88
Aliou Diallo <code@aliou.me>
2015-03-11 19:07:11 +0100

    Add ruby migrate function.

--- code/ruby/aliases.sh
+++ code/ruby/aliases.sh
@@ -7,4 +7,12 @@ alias reload!='echo "Wrong console, dummy."'
 # Edit last migration.
 alias emig='vim db/migrate/$(\ls -1 db/migrate/ | tail -n 1)'
 
+migrate() {
+  if [[ -f './bin/rake' ]]; then
+    ./bin/rake db:migrate
+  else
+    bundle exec rake db:migrate
+  fi
+}
+
 alias powrestart='touch ~/.pow/restart.txt'
