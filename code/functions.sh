930804f54d4d59dad9081539b8075a794e2aadcd
Aliou Diallo <code@aliou.me>
2015-11-08 01:44:17 +0100

    DRYed up some bash functions.

--- code/functions.sh
+++ code/functions.sh
@@ -1,73 +1,73 @@
 #!/usr/bin/env bash
-# TODO: DRY `j` and `p.`
 # Requirements:
-# - pick <http://git.io/pick> or fzf <http://git.io/C4FBDw> or selecta <http://git.io/oGBVlw>
+# - pick <http://git.io/pick> or fzf <http://git.io/C4FBDw> or
+#   selecta <http://git.io/oGBVlw>
 # - ack
 
 # Easily switch fuzzy finder by setting the FUZZY_CMD env variable.
 export FUZZY_CMD="fzf --reverse"
 
-# Jump to a subfolder.
-# Usage: j [ folder ]
-# TODO: Ignore common folders.
-# http://stackoverflow.com/questions/4210042/exclude-directory-from-find-command
-j() {
-  FOLDER=${1:-.*}
-  IGNORED_FOLDERS=('node-modules')
-
-  FOLDERS=$(find . -maxdepth 5 -type d | ack -v '.git' --nocolor $FOLDER)
-  MATCHES_COUNT=$(echo $FOLDERS | tr ' ' '\n' | wc -l)
-
-  TARGET=
-
-  if [[ ! $FOLDER ]]; then
-    TARGET=$(find -L . -maxdepth 5 -type d | $FUZZY_CMD)
-  else
-    if (( $MATCHES_COUNT == 1 )); then
-      TARGET=$FOLDERS
-    elif (( $MATCHES_COUNT > 1 )); then
-      TARGET=$(find -L -s $FOLDERS -maxdepth 0 -type d | $FUZZY_CMD)
-    fi
-  fi
-
-  if [[ -n $TARGET ]]; then
-    cd $TARGET && folder_info
-  fi
-}
-
 # Jump to a project file.
-# Usage: p [ project ]
+# Usage: p [ project_name ]
+# TODO: Ignore common folders.
 p() {
+  if [[ -z $__P_PROJECT_FOLDERS ]]; then
+    >&2 echo 'The environment variable `__P_PROJECT_FOLDERS` is not defined.'
+    >&2 echo 'Please define it to use `p`.'
+    return 1
+  fi
+
   PROJECT=${1:-.*}
 
-  FOLDERS=" \
-    $HOME/src/github/**/* \
-    $HOME/src/heroku/**/* \
-    $HOME/src/.go/src/**/aliou/*"
+  FOLDERS=$__P_PROJECT_FOLDERS
+  DEPTH=${__P_MAX_DEPTH:-0}
 
-  PROJECTS=$(find -L -s $FOLDERS -maxdepth 0 -type d | ack --nocolor $PROJECT)
+  # Filter the projects using the name passed as argument.
+  PROJECTS=$(
+    find -L -s $FOLDERS -maxdepth $DEPTH -type d | \
+      uniq | \
+      ack --nocolor $PROJECT
+  )
   MATCHES_COUNT=$(echo $PROJECTS | tr ' ' '\n' | wc -l)
 
   TARGET=
 
+  # If the filtering returns nothing, let the user choose between every projects.
   if [[ ! $PROJECTS ]]; then
-    TARGET=$(find -L -s $FOLDERS -maxdepth 0 -type d | $FUZZY_CMD)
-  else
-    if (( $MATCHES_COUNT == 1 )); then
-      TARGET=$PROJECTS
-    elif (( $MATCHES_COUNT > 1 )); then
-      TARGET=$(find -L -s $PROJECTS -maxdepth 0 -type d | $FUZZY_CMD)
-    fi
+    TARGET=$(
+      find -L -s $FOLDERS -maxdepth $DEPTH -type d | $FUZZY_CMD
+    )
+    # If there is only one match we're done.
+  elif (( $MATCHES_COUNT == 1 )); then
+    TARGET=$PROJECTS
+    # If there are several matches, let the user choose between the matches.
+  elif (( $MATCHES_COUNT > 1 )); then
+    TARGET=$(
+      find -L -s $PROJECTS -maxdepth $DEPTH -type d | $FUZZY_CMD
+    )
   fi
 
   if [[ -n $TARGET ]]; then
     cd $TARGET && folder_info
   fi
-
 }
 
+# Jump to a subfolder.
+# Usage: j [ folder ]
+# http://stackoverflow.com/questions/4210042/exclude-directory-from-find-command
+j() {
+  local __P_PROJECT_FOLDERS="."
+  local __P_MAX_DEPTH=5
+  local __P_IGNORED_FOLDERS=( '.git' 'node_modules' )
+  p $@
+}
+
+
 e() {
-  vim $(find . -maxdepth 3 -type f ! -path '*/.git*' ! -path '*/.hg*' ! -path '*/.svn*' | $FUZZY_CMD) && clear
+  vim $(
+    find . -maxdepth 3 -type f \
+    ! -path '*/.git*' ! -path '*/.hg*' ! -path '*/.svn*' | $FUZZY_CMD
+  ) && clear
 }
 
 folder_info() {
