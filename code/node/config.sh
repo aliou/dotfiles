141972644103b1424914ee26691f64661bc3673d
Aliou Diallo <code@aliou.me>
2015-11-30 11:48:16 +0100

    Only source nvm stuff when nvm is present.

--- code/node/config.sh
+++ code/node/config.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
 # Setup Node Version Manager.
-export NVM_DIR="/Users/alioudiallo/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+if [[ -n `which nvm` ]]; then
+  export NVM_DIR="/Users/alioudiallo/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+fi
