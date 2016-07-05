e4d76623ccec0a4549c2f6209166836fe2185faa
Aliou Diallo <aliou@followanalytics.com>
2016-07-05 10:07:17 +0200

    Remove unused docker aliases.

--- code/docker/config.sh
+++ code/docker/config.sh
@@ -1,12 +1,2 @@
 #!/usr/bin/env bash
-
-if [[ -n `which docker-compose` ]]; then
-  alias dcp='docker-compose'
-  alias dcpl='docker-compose logs'
-  alias dcpu='docker-compose up'
-  alias dcpr='docker-compose restart'
-fi
-
-if [[ -n `which docker-machine` ]]; then
-  alias dcm='docker-machine'
-fi
+# Used to hold docker aliases.
