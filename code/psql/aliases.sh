3d5a1f8be16676f9fbb885b63aa6a79e84412910
Aliou Diallo <code@aliou.me>
2016-11-20 12:59:31 +0100

    Use `pgcli` instead of `psql` when available.

--- code/psql/aliases.sh
+++ code/psql/aliases.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+if [[ -n `which pgcli` ]]; then
+  alias psql='pgcli'
+fi
+
 # When running postgres in Docker.
 # Overriden in some projects local configuration.
 alias dpsql='psql -h localhost'
