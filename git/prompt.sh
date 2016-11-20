8578207eba7003fc5e9c1a473857f012b6f70e33
Aliou Diallo <code@aliou.me>
2016-11-20 13:00:59 +0100

    Show when the branch is has a wip commit.

--- git/prompt.sh
+++ git/prompt.sh
@@ -14,9 +14,15 @@ function parse_git_stashed {
     stashed=$(`which git` stash list 2> /dev/null | wc -l | sed 's/ //g')
     [[ $stashed -ne 0 ]] && echo "[$stashed]"
 }
+
+function parse_git_wip {
+  wip=$(git log --graph --pretty=format:'%s' -1 2> /dev/null | \grep -i wip | wc -l | sed 's/ //g')
+  [[ $wip -ne 0 ]] && echo "[WIP]"
+}
+
 function parse_git_dirty {
     echo "$(parse_git_added)$(parse_git_modified)$(parse_git_deleted)"
 }
 function parse_git_branch {
-    `which git` branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]$(parse_git_stashed) /"
+  `which git` branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]$(parse_git_stashed)$(parse_git_wip) /"
 }
