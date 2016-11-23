a275b3164a544436aaf7d9d47be8f3dbdb029e72
Aliou Diallo <code@aliou.me>
2016-11-23 17:06:16 +0100

    Extract `history` options into their own file.

---
+++ system/history.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+# Disable the entry limit for the history file.
+export HISTSIZE=
+
+# Disable file size limit for the history file.
+export HISTFILESIZE=
+
+# History date format.
+export HISTTIMEFORMAT='%b %d %I:%M %p '
+
+# Ignore those commands in history.
+export HISTIGNORE="history:pwd:exit:df:ls:update:fg:env:cdd:cdv:h:gh"
+
+# don't put duplicate lines in the history. See bash(1) for more options
+# ... or force ignoredups and ignorespace
+export HISTCONTROL=ignoredups:ignorespace
