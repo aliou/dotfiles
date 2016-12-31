733f46279e5991d176d14cb78b52f8aececf437f
Aliou Diallo <code@aliou.me>
2017-01-01 00:54:48 +0100

    Remove unused aliases.

--- system/aliases.sh
+++ system/aliases.sh
@@ -7,38 +7,19 @@ alias ls="ls -lAGh"
 alias l1='tree --dirsfirst -ChFLa 1 -I ".git*|tags"'
 alias l2='tree --dirsfirst -ChFLa 2 -I ".git*|tags"'
 alias l3='tree --dirsfirst -ChFLa 3 -I ".git*|tags"'
-alias l4='tree --dirsfirst -ChFLa 4 -I ".git*|tags"'
-alias l="l1"
 
-alias c='cd ..'
+alias cdb='cd ~/.dotbin'
 alias cdd='cd ~/.dotfiles'
+alias cdg='cd ~/code/src/github.com/aliou/'
 alias cds='cd ~/code/src'
 alias cdt='cd ~/code/tmp'
-alias cdg='cd ~/code/src/github.com/aliou/'
 alias cdv='cd ~/.dotvim'
 
 # Reloads the shell without having PATH re-adding to himself.
 alias update="unset PATH ; exec $SHELL"
-
-alias su="su -"
-
-sudoWisdom=(
-"ONE COMMAND TO RULE THEM ALL."
-"With great power, comes great responsibility."
-"One does not simply become Super User."
-"TREAT. YO. SELF."
-'Ray, when someone asks if you are a god, you say, "Yes!"'
-"HATERS GONNA DO THAT THING THEY ARE EPONYMOUSLY REPUTED TO DO."
-)
-
-#For the lulz.
-alias sudo="echo ${sudoWisdom[$RANDOM % 6]} ; sudo"
 
 # Herping and Derping
 if [ -x $(which rg) ]; then
   alias grep=rg
   alias ack=rg
 fi
-
-alias first='head -n 1'
-alias last='tail -n 1'
