719be7eb15cffedd1d4beeaa93b973f8aded66b7
Aliou Diallo <code@aliou.me>
2017-01-31 06:51:39 +0100

    Add marker when a Vim session file is present in cwd.

--- system/prompt.sh
+++ system/prompt.sh
@@ -1,4 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
+function has_session() {
+  [[ -f "$PWD/Session.vim" ]] && echo '[$] '
+}
 
 PROMPT_COMMAND='hasjobs=$(jobs -p)'
-export PS1="\[$bldblu\]\${hasjobs:+[\j] }\[$bldred\]\h: \[$txtwht\]\W \[$bldblu\]\$(parse_git_branch)\[\e[0m\]› "
+export PS1="\[$bldblu\]\${hasjobs:+[\j] }\[$bldred\]\h: \[$txtwht\]\W \[$bldblu\]\$(parse_git_branch)\[\e[0m\]\$(has_session)› "
+
+# vim: tw=0
