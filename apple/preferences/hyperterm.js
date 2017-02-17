b8a5b7467eeba91af3685237a4b9ab43c59c72a5
Aliou Diallo <code@aliou.me>
2017-02-17 16:30:55 +0100

    Update hyperterm configuration.

--- apple/preferences/hyperterm.js
+++ apple/preferences/hyperterm.js
@@ -4,7 +4,7 @@ module.exports = {
     fontSize: 12,
 
     // font family with optional fallbacks
-    fontFamily: 'SF Mono Regular, Menlo, "DejaVu Sans Mono", "Lucida Console", monospace',
+    fontFamily: 'SFMono-Regular',
 
     // terminal cursor background color and opacity (hex, rgb, hsl, hsv, hwb or cmyk)
     cursorColor: '#AAA',
@@ -28,7 +28,7 @@ module.exports = {
     termCSS: '',
 
     // custom padding (css format, i.e.: `top right bottom left`)
-    padding: '1px 1px 1px 1px',
+    padding: '4px 4px 4px 4px',
 
     // the full list. if you're going to provide the full color palette,
     // including the 6 x 6 color cubes and the grayscale map, just provide
@@ -38,7 +38,7 @@ module.exports = {
       red: '#ff0000',
       green: '#33ff00',
       yellow: '#ffff00',
-      blue: '#0066ff',
+      blue: '#396FAA',
       magenta: '#cc00ff',
       cyan: '#00ffff',
       white: '#d0d0d0',
@@ -52,12 +52,9 @@ module.exports = {
       lightWhite: '#ffffff'
     },
 
-    // the shell to run when spawning a new session (i.e. /usr/local/bin/fish)
-    // if left empty, your system's login shell will be used by default
-    shell: '',
+    shell: '/usr/local/bin/bash',
 
-    // for advanced config flags please refer to https://hyperterm.org/#cfg
-
+    // Disable bell sound because FUCK THAT.
     bell: false,
   },
 
@@ -68,10 +65,12 @@ module.exports = {
   //   `@company/project`
   //   `project#1.0.1`
   plugins: [
+    'hyperminimal',
+    'hyper-snazzy',
   ],
 
   // in development, you can create a directory under
   // `~/.hyperterm_plugins/local/` and include it here
   // to load it and avoid it being `npm install`ed
-  localPlugins: []
+  localPlugins: [],
 };
