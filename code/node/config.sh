27694c0344a9c7c69e479754293cabd584b147b8
Aliou Diallo <code@aliou.me>
2017-04-06 20:17:35 +0200

    Use local yarn.

--- code/node/config.sh
+++ code/node/config.sh
@@ -2,4 +2,6 @@
 
 # Setup Node Version Manager.
 export NVM_DIR="/Users/alioudiallo/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && . "$NVM_DIR/nvm.sh"
+[ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+
+[ -d "$HOME/.yarn/bin" ] && export PATH="$HOME/.yarn/bin:$PATH"
