51ba21d0b4fac47b8e7663fb1e78604b37d26e33
Aliou Diallo <code@aliou.me>
2017-04-13 07:40:41 +0200

    Manually add `exenv` bash completion.

--- code/zelixir/config.sh
+++ code/zelixir/config.sh
@@ -1,6 +1,25 @@
 #!/usr/bin/env bash
 
+# Generate completions for exenv command.
+_exenv() {
+  COMPREPLY=()
+  local word="${COMP_WORDS[COMP_CWORD]}"
+
+  if [ "$COMP_CWORD" -eq 1 ]; then
+    COMPREPLY=( $(compgen -W "$(exenv commands)" -- "$word") )
+  fi
+  # TODO: Create `exenv-completions` that gives completions for the other
+  # commands.
+  # else
+  #   local command="${COMP_WORDS[1]}"
+  #   local completions="$(exenv completions "$command")"
+  #   COMPREPLY=( $(compgen -W "$completions" -- "$word") )
+  # fi
+}
+
 if [[ -n $(which exenv) ]]; then
   export EXENV_ROOT="$HOME/.exenv"
   eval "$(exenv init -)"
+
+  complete -F _exenv exenv
 fi
