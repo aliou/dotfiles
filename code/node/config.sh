a07a413f85065e09d4ef2fea91b3089f03455b2c
Aliou Diallo <code@aliou.me>
2017-06-18 17:14:22 +0200

    Use brew version of `yarn`.

--- code/node/config.sh
+++ code/node/config.sh
@@ -3,5 +3,3 @@
 # Setup Node Version Manager.
 export NVM_DIR="/Users/alioudiallo/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
-
-[ -d "$HOME/.yarn/bin" ] && export PATH="$HOME/.yarn/bin:$PATH"
