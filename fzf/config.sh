8c0f2197e917e188d3de2c7e9085cf19bf625faf
Aliou Diallo <code@aliou.me>
2017-08-23 12:11:56 +0200

    Add fzf completion and key binding.

--- fzf/config.sh
+++ fzf/config.sh
@@ -1,3 +1,11 @@
 #!/usr/bin/env bash
 
 export FZF_DEFAULT_OPTS='--height 40% --reverse --color=light -1'
+
+# Auto-completion
+# ---------------
+[[ $- == *i* ]] && source "/usr/local/opt/fzf/shell/completion.bash" 2> /dev/null
+
+# Key bindings
+# ------------
+source "/usr/local/opt/fzf/shell/key-bindings.bash"
