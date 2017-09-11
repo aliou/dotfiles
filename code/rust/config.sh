87acc56e464fd18de1cf118b832efb25b1aaba42
Aliou Diallo <code@aliou.me>
2017-09-11 22:22:38 +0200

    Setup RUST_SRC_PATH environment variable.

--- code/rust/config.sh
+++ code/rust/config.sh
@@ -3,4 +3,11 @@
 # Load rust and other binaries.
 if [[ -d "$HOME/.cargo" ]]; then
   export PATH="$HOME/.cargo/bin:$PATH"
+
+  # Set the src path for Racer (Rust Code Completion utility) and rls (Rust
+  # Language Server).
+  # This requires to have the rust source installed.
+  #   => rustup component add rust-src
+  export RUST_SRC_PATH
+  RUST_SRC_PATH="$(rustc --print sysroot)/lib/rustlib/src/rust/src"
 fi
