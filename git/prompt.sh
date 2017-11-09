f17e14909bfd7b448f3b72d30788eb9cc8266e07
Aliou Diallo <code@aliou.me>
2017-11-09 05:50:44 +0100

    [git] Simplify current branch status check.

--- git/prompt.sh
+++ git/prompt.sh
@@ -1,29 +1,11 @@
 #!/bin/bash
 # https://gist.github.com/48207
 
-function parse_git_deleted {
-    [[ $($(which git) status 2> /dev/null | grep deleted:) != "" ]] && echo "-"
-}
-function parse_git_added {
-    [[ $($(which git) status 2> /dev/null | grep "Untracked files:") != "" ]] && echo '+'
-}
-function parse_git_modified {
-    [[ $($(which git) status 2> /dev/null | grep modified:) != "" ]] && echo "#"
-}
-function parse_git_stashed {
-    stashed=$($(which git) stash list 2> /dev/null | wc -l | sed 's/ //g')
-    [[ $stashed -ne 0 ]] && echo "[$stashed]"
-}
-
-function parse_git_wip {
-  GREP=$(which grep)
-  wip=$(git log --pretty=format:'%s' -1 2> /dev/null | $GREP -ic ^wip)
-  [[ $wip -ne 0 ]] && echo "[WIP]"
-}
-
-function parse_git_dirty {
-    echo "$(parse_git_added)$(parse_git_modified)$(parse_git_deleted)"
-}
 function parse_git_branch {
-  $(which git) branch --no-color 2> /dev/null | sed -e '/^[^*]/d' -e "s/* \(.*\)/[\1$(parse_git_dirty)]$(parse_git_stashed)$(parse_git_wip) /"
+  {
+    echo '['
+    git branch --format="%(refname:short)" 2> /dev/null
+    git status --porcelain 2> /dev/null | cut -b 2 | sort | uniq | tr '?MD' '+#-'
+    echo ']'
+  } | xargs | sed 's/ //g'
 }
