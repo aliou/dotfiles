04c036ba42fe80933df383c4d37d0d7462fffbb9
Aliou Diallo <code@aliou.me>
2017-12-21 09:26:30 +0100

    [android] Add android SDK root.

--- code/android/config.sh
+++ code/android/config.sh
@@ -3,4 +3,5 @@
 if [[ -d "/usr/local/opt/android-sdk" ]]; then
   export ANDROID_HOME="/usr/local/opt/android-sdk"
   export PATH="${PATH}:${ANDROID_HOME}/bin"
+  export ANDROID_SDK_ROOT="/usr/local/share/android-sdk"
 fi
