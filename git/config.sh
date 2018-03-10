577e4b0647d75bec4ea87ac616bf7b87096a39c6
Aliou Diallo <code@aliou.me>
2018-03-10 19:38:34 +0100

    [git] Add vim-sensbile to git editor.

--- git/config.sh
+++ git/config.sh
@@ -1,2 +1,2 @@
 # Set a lighther editor when editing in git (commits, interactive rebase, etc).
-export GIT_EDITOR="vim --clean -c 'set tw=72 | color hybrid | set bg=dark | set spell'"
+export GIT_EDITOR="vim --clean -c 'set tw=72 | packadd sensible | color hybrid | set bg=dark | set spell'"
