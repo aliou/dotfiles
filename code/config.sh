8f8be8628b315c243de6142f9b4c997a49d10f94
Aliou Diallo <code@aliou.me>
2018-05-01 21:14:42 +0200

    [code] Remove unused stuff.

--- code/config.sh
+++ code/config.sh
@@ -6,25 +6,5 @@ do
   source $config_file
 done
 
-alias :w='echo "Not in \$EDITOR, dummy"'
-alias :e="vim"
-
 alias v="vim"
 alias vs="vim -S Session.vim"
-
-alias cdp='cd ~/code/src/github.com/aliou/'
-
-# Change terminal title and open tmuxinator config.
-# TODO: Check if the tmuxinator config is present.
-# TODO: Either retrain my muscle memory or don't change the title when sending a
-# tmuxinator command instead of a project. e.g. `m list` should run
-# `tmuxinator list` without changing the terminal name.
-function m() {
-  if (( $# != 1 )); then
-    return 1
-  fi
-  # Change terminal name on OS X.
-  echo -ne "\033]0;" "$*" "\007"
-
-  tmuxinator "$*"
-}
