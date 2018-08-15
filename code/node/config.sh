591aa420c7c0ccf5986aaea49aaada2804412e4b
Aliou Diallo <code@aliou.me>
2018-08-15 07:50:07 +0200

    [code/node] FOH nvm

--- code/node/config.sh
+++ code/node/config.sh
@@ -2,7 +2,7 @@
 # shellcheck source=/dev/null
 
 # Setup Node Version Manager.
-if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
-  export NVM_DIR="/Users/alioudiallo/.nvm"
-  source "$NVM_DIR/nvm.sh"
-fi
+# if [[ -s "$HOME/.nvm/nvm.sh" ]]; then
+#   export NVM_DIR="/Users/alioudiallo/.nvm"
+#   source "$NVM_DIR/nvm.sh"
+# fi
