e6d93eac669e4d52a7611089bd95723422817899
Aliou Diallo <code@aliou.me>
2019-02-15 16:04:41 +0100

    [tmux] Simplify attach function.

--- tmux/aliases.sh
+++ tmux/aliases.sh
@@ -1,37 +1,18 @@
 #!/usr/bin/env bash
+
 alias tmux="tmux -2"
 alias td="tmux detach"
 alias tk="tmux kill-session"
 alias tl="tmux list-session"
-alias tm='tmux'
 
-[[ -z $FUZZY_CMD ]] && export FUZZY_CMD="pick"
+[[ -z $FUZZY_CMD ]] && export FUZZY_CMD="fzf"
 
-function ts() {
-  COUNT=$(tmux list-sessions | wc -l)
-  if (( $COUNT == 1 )); then
-    tmux switch-client
-  else
-    if [[ $1 == "" ]]; then
-      SESSION=$(tmux list-sessions -F '#S' | $FUZZY_CMD)
-    else
-      SESSION=$@
-    fi
-
-    tmux switch-client -t "$SESSION"
-  fi
-}
+alias ts='tmuxifier load-session'
 
 function ta() {
-  COUNT=$(tmux list-sessions | wc -l)
-  if (( $COUNT == 1 )); then
-    tmux at
-  else
-    if [[ $1 == "" ]]; then
-      SESSION=$(tmux list-sessions -F '#S' | $FUZZY_CMD)
-    else
-      SESSION=$@
-    fi
+  COUNT=$(tmux list-sessions 2> /dev/null | \grep '^.*:.*' | wc -l)
+  if (( $COUNT > 1 )); then
+    SESSION=$(tmux list-sessions -F '#S' | $FUZZY_CMD -1)
 
     tmux at -t "$SESSION"
   fi
