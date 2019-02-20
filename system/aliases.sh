e959ead4c44594d697d958b1e916c0535e24aadf
Aliou Diallo <code@aliou.me>
2019-02-20 19:22:44 +0100

    [system] Remove unused aliases.

--- system/aliases.sh
+++ system/aliases.sh
@@ -27,9 +27,3 @@ alias cdv='cd ~/.dotvim'
 # Reloads the shell without having PATH re-adding to himself.
 alias update="unset PATH ; exec \$SHELL"
 alias reset="\reset; echo 'terminfo database reset, maybe you meant to use \`update\`?'"
-
-# Herping and Derping
-if [ -x "$(command -v rg)" ]; then
-  alias grep="echo 'use rg or \\grep'"
-  alias ack="echo 'use rg or \\ack'"
-fi
