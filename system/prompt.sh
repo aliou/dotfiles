935648f85b06e5c2629ab53119dfa4ddcef560c3
Aliou Diallo <code@aliou.me>
2019-07-06 23:05:05 +0200

    [system] Use impel as prompt.

--- system/prompt.sh
+++ system/prompt.sh
@@ -5,6 +5,8 @@ function has_session() {
 }
 
 PROMPT_COMMAND='hasjobs=$(jobs -p)'
-export PS1="\[$bldblu\]\${hasjobs:+[\j] }\[$bldred\]\h: \[$txtrst\]\W \[$bldblu\]\$(parse_git_branch)\[\e[0m\]\$(has_session)› "
+# export PS1="\[$bldblu\]\${hasjobs:+[\j] }\[$bldred\]\h: \[$txtrst\]\W \[$bldblu\]\$(parse_git_branch)\[\e[0m\]\$(has_session)› "
+
+export PS1="\[$bldblu\]\${hasjobs:+[\j] }\$(impel)"
 
 # vim: tw=0
