d275e8850764c093b81601bebd49f6f1ce842eba
Aliou Diallo <code@aliou.me>
2020-02-27 06:39:22 +0100

    [system] Display the number of background jobs.

--- system/prompt.sh
+++ system/prompt.sh
@@ -1,9 +1,7 @@
 #!/usr/bin/env bash
 # shellcheck disable=SC2154
 
-prompt_cmd() {
-  local hasjobs=$(jobs -p)
-  PS1="\[$bldblu\]\${hasjobs:+[\j] }\[\$(impel)\]\[$txtrst\]\n› "
-}
+PROMPT_COMMAND='hasjobs=$(jobs -p)'
+export PS1="\[$bldblu\]\${hasjobs:+[\j] }\[\$(impel)\]\[$txtrst\]\n› "
 
-PROMPT_COMMAND=prompt_cmd
+# vim: tw=0
