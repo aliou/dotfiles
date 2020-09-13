1e2aad5be4f979e0564e89f0154d2781c80367d7
Aliou Diallo <code@aliou.me>
2020-09-13 21:02:58 +0200

    [code/ruby] Try to do the right thing regarding bins.

--- code/ruby/config.sh
+++ code/ruby/config.sh
@@ -1,7 +1,22 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-if [[ -n $(which rbenv) ]]; then
+if [[ -n $(command -v rbenv) ]]; then
   # Only works if rbenv is installed from brew, be careful.
   export RBENV_ROOT="/usr/local/var/rbenv"
   eval "$(rbenv init -)"
 fi
+
+for cmd in jekyll rails rubocop rspec sidekiq; do
+  eval << EOF
+    $cmd() {
+      local exe = \$(which $cmd)
+      if [ -x "./bin/$cmd" ]; then
+        ./bin/$cmd \"\$@\"
+      elif [ -f "./Gemfile" ]; then
+        bundle exec $cmd \"\$@\"
+      else
+        command $cmd \"\$@\"
+      fi
+    }
+EOF
+done
