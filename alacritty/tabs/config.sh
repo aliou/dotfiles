cb5b22f8ac321bebe405da5038a734005f95b7b0
Aliou Diallo <code@aliou.me>
2020-11-28 11:08:03 +0100

    [alacritty/tabs] Improve variable name.

--- alacritty/tabs/config.sh
+++ alacritty/tabs/config.sh
@@ -8,8 +8,8 @@ function atabs() {
 
 # Allow creating "regular" tmux sessions.
 if [[ -n "$ALACRITTY" ]] && [[ -n "$TMUX" ]]; then
-  session_name=$(echo "$TMUX" | cut -f1 -d',' | cut -d'/' -f5)
-  if [[ "$session_name" = "alacritty" ]]; then
+  socket_name=$(echo "$TMUX" | cut -f1 -d',' | cut -d'/' -f5)
+  if [[ "$socket_name" = "alacritty" ]]; then
     unset TMUX
   fi
 fi
