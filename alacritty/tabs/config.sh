b8af85814a6c253a7c501ab7f4baf03c52702c7c
Aliou Diallo <code@aliou.me>
2020-11-28 11:03:35 +0100

    [alacritty/tabs] Remove unused logic.

--- alacritty/tabs/config.sh
+++ alacritty/tabs/config.sh
@@ -23,14 +23,6 @@ function atabs() {
   [[ -n $ALACRITTY ]] && tmux -f "$_ATABS_CONFIGURATION" -L alacritty $@
 }
 
-if [[ -n "$ALACRITTY" ]] && [[ -z "$TMUX" ]]; then
-
-  # TODO: Each window has its own tmux-alacritty session.
-  # Ideally, get the session name from the environment, no idea how tho.
-  atabs attach 2> /dev/null || atabs new-session
-  exit
-fi
-
 # Allow creating "regular" tmux sessions.
 if [[ -n "$ALACRITTY" ]] && [[ -n "$TMUX" ]]; then
   session_name=$(echo "$TMUX" | cut -f1 -d',' | cut -d'/' -f5)
