9bb60c3f96365ec892a007248cba6d13ac426fbe
Aliou Diallo <code@aliou.me>
2020-12-05 14:10:50 +0100

    [alacritty/tabs] Rename function in prevision of CLI.

--- alacritty/tabs/config.sh
+++ alacritty/tabs/config.sh
@@ -1,14 +1,23 @@
 #!/usr/bin/env bash
 
-export _ATABS_CONFIGURATION="$HOME/.dotfiles/alacritty/tabs/tmux-tabs.conf"
+export _TAB_CONFIGURATION="$HOME/.dotfiles/alacritty/tabs/tmux-tabs.conf"
 
-function atabs() {
-  [[ -n $ALACRITTY ]] && tmux -f "$_ATABS_CONFIGURATION" -L alacritty $@
+function tab() {
+  [[ -n $ALACRITTY ]] && tmux -f "$_TAB_CONFIGURATION" -L alacritty $@
+}
+
+function rename-window() {
+  tab rename-window "$@"
+}
+
+function rename-session() {
+  tab rename-session "$@"
 }
 
 # Allow creating "regular" tmux sessions.
 if [[ -n "$ALACRITTY" ]] && [[ -n "$TMUX" ]]; then
   socket_name=$(echo "$TMUX" | cut -f1 -d',' | cut -d'/' -f5)
+
   if [[ "$socket_name" = "alacritty" ]]; then
     unset TMUX
   fi
