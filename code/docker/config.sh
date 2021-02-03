51196e1a3c0758ca230a5f7069fffe3ca4235612
Aliou Diallo <code@aliou.me>
2021-02-03 11:13:16 +0100

    [code/docker] Add back docker compose aliases.

--- code/docker/config.sh
+++ code/docker/config.sh
@@ -1,3 +1,10 @@
 #!/usr/bin/env bash
 
+if [[ -n `which docker-compose` ]]; then
+  alias dcp='docker-compose'
+  alias dcpl='docker-compose logs -f'
+  alias dcpu='docker-compose up'
+  alias dcpr='docker-compose run --rm'
+fi
+
 export WHALEBREW_INSTALL_PATH="$HOME/code/bin"
