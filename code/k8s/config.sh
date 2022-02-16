bda29c03593b214b6277ee17055feb6c2b520446
Aliou Diallo <code@aliou.me>
2022-02-16 15:44:13 +0100

    [code/k8s] Add k8s aliases.

---
+++ code/k8s/config.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+alias k=kubectl
+alias kgp='kubectl get pods'
+alias kgd='kubectl get deployments'
+
+# TODO: Find a way to refactor the aliases below?
+alias mk='minikube kubectl --'
+alias mkgp='minikube kubectl -- get pods'
+alias mkgd='minikube kubectl -- get deployments'
