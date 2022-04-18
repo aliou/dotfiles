9eaaeabcda77a5decd162fa26e622339824770ce
Aliou Diallo <code@aliou.me>
2022-04-18 19:39:34 +0200

    [keyboards/milky_neko] Add notes for next steps.

--- keyboards/milky_neko/keymap.c
+++ keyboards/milky_neko/keymap.c
@@ -5,6 +5,11 @@ enum layer_names {
     _FN1,
 };
 
+// Prefix custom keycodes by `MN` to prevent colisions.
+enum alt_keycodes {
+  MN_DFU = SAFE_RANGE
+};
+
 // Tap for ESC, hold for CTRL.
 #define CTL_ESC  LCTL_T(KC_ESC)
 
@@ -21,14 +26,42 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL,  KC_LALT, KC_LGUI,                            KC_SPC,                            MO(_FN1), KC_RGUI, KC_RALT, KC_RCTL
   ),
 
-  // WASD for arrow keys + B for DFU mode (to match what we do for other personal keyboards).
+  // WASD for arrow keys + B for DFU mode.
   [_FN1] = LAYOUT_60_ansi(
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,
     _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______,  _______,          _______,
-    _______, _______, _______, _______, _______, RESET,   _______, _______, _______, _______, _______,                    _______,
+    _______, _______, _______, _______, _______, MN_DFU,  _______, _______, _______, _______, _______,                    _______,
     _______, _______, _______,                            _______,                            _______,  _______, _______, _______
   ),
 };
 
-// vim:foldmethod=syntax tw=0
+// TODO:
+//   - Selecting words keymaps:
+//     - LALT + LSFT + arrows
+//   - Moom keystrokes that don't work because of the lack of arrow keys:
+//     - LCTL + LALT + LSFT + arrow up (replaced by rsft)
+//     - LCTL + LALT + LSFT + arrow down (replaced by rctl)
+//   - Test a way to trigger a layer with my left hand and type arrows with my right hand (e.g. KC_GRAVE or KC_TAB + HJKL)
+bool process_record_user(uint16_t keycode, keyrecord_t *record) {
+  static uint32_t key_timer;
+
+  switch (keycode) {
+    // Put keyboard in DFU mode when pression the combinaison for more than 500ms.
+    case MN_DFU:
+      if (record->event.pressed) {
+        key_timer = timer_read32();
+      } else {
+        if (timer_elapsed32(key_timer) >= 500) {
+          reset_keyboard();
+        }
+      }
+      return false;
+
+    // Process all other keycodes normally.
+    default:
+      return true;
+  }
+}
+
+// vim:foldmethod=syntax tw=0 shiftwidth=2
--- keyboards/milky_neko/rules.mk
+++ keyboards/milky_neko/rules.mk
@@ -1,1 +1,3 @@
-# VIA_ENABLE = yes
+# No particular need to have Via mode, kept here to quickly
+# enable it to test the PCB.
+VIA_ENABLE = no
