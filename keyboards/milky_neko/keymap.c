8f913fbf0236575b43d4584c9d2bfdcc9dd91ecc
Aliou Diallo <code@aliou.me>
2022-04-19 23:03:10 +0200

    [keyboards/milky_neko] Add additional mapping for arrow keys.

--- keyboards/milky_neko/keymap.c
+++ keyboards/milky_neko/keymap.c
@@ -1,8 +1,8 @@
 #include QMK_KEYBOARD_H
 
 enum layer_names {
-    _BASE,
-    _FN1,
+  _BASE,
+  _FN1,
 };
 
 // Prefix custom keycodes by `MN` to prevent colisions.
@@ -31,18 +31,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,  KC_F12,  KC_DEL,
     _______, _______, KC_UP,   _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______,
     _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, _______,  _______,          _______,
-    _______, _______, _______, _______, _______, MN_DFU,  _______, _______, _______, _______, _______,                    _______,
-    _______, _______, _______,                            _______,                            _______,  _______, _______, _______
+    _______, _______, _______, _______, _______, MN_DFU,  _______, _______, _______, _______, _______,                    KC_UP,
+    _______, _______, _______,                            _______,                            _______,  KC_LEFT, KC_DOWN, KC_RGHT
   ),
 };
 
-// TODO:
-//   - Selecting words keymaps:
-//     - LALT + LSFT + arrows
-//   - Moom keystrokes that don't work because of the lack of arrow keys:
-//     - LCTL + LALT + LSFT + arrow up (replaced by rsft)
-//     - LCTL + LALT + LSFT + arrow down (replaced by rctl)
-//   - Test a way to trigger a layer with my left hand and type arrows with my right hand (e.g. KC_GRAVE or KC_TAB + HJKL)
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   static uint32_t key_timer;
 
