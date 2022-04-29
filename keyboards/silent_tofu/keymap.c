df5148e73a5ac1d73e0ba1153075f9ea84f07acf
Aliou Diallo <code@aliou.me>
2022-04-30 00:09:56 +0200

    [keyboards/silent_tofu] Bunch of tweaks.
    - Add back KC_SLSH key on the right of Right Shift.
    - Move volume maps to match with milky_neko.
    - Rename layers to match with mily_neko.

--- keyboards/silent_tofu/keymap.c
+++ keyboards/silent_tofu/keymap.c
@@ -1,8 +1,8 @@
 #include QMK_KEYBOARD_H
 
 enum layers {
-  _BL = 0, // Base layer
-  _FL = 1, // Function layer
+  _BASE = 0,
+  _FN1 = 1,
 };
 
 // Will use LK as a prefix for custom keycodes.
@@ -15,19 +15,19 @@ enum alt_keycodes {
 #define SHT_SLH  RSFT_T(KC_SLSH)   // Tap for right SHIFT, hold for Slash.
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-    [_BL] = LAYOUT(
-        KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  KC_6,    KC_7,    KC_8,     KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
-        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  KC_Y,    KC_U,    KC_I,     KC_O,    KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
-        CTL_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_H,    KC_J,    KC_K,     KC_L,    KC_SCLN, KC_QUOT,          KC_ENT,
-        KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,     KC_COMM, KC_DOT,  SHT_SLH, KC_UP,   KC_DEL,
-        KC_LCTL,  KC_LALT, KC_LGUI,                          KC_SPC,                     KC_RGUI, MO(_FL), KC_LEFT, KC_DOWN, KC_RGHT
+    [_BASE] = LAYOUT(
+        KC_GRAVE, KC_1,    KC_2,    KC_3,    KC_4,    KC_5,  KC_6,    KC_7,    KC_8,     KC_9,     KC_0,    KC_MINS, KC_EQL,  KC_BSPC,
+        KC_TAB,   KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,  KC_Y,    KC_U,    KC_I,     KC_O,     KC_P,    KC_LBRC, KC_RBRC, KC_BSLS,
+        CTL_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,  KC_H,    KC_J,    KC_K,     KC_L,     KC_SCLN, KC_QUOT,          KC_ENT,
+        KC_LSFT,           KC_Z,    KC_X,    KC_C,    KC_V,  KC_B,    KC_N,    KC_M,     KC_COMM,  KC_DOT,  SHT_SLH, KC_UP,   KC_SLSH,
+        KC_LCTL,  KC_LALT, KC_LGUI,                          KC_SPC,                     MO(_FN1), KC_RGUI, KC_LEFT, KC_DOWN, KC_RGHT
     ),
-    [_FL] = LAYOUT(
-        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  CMD_BPC,
+    [_FN1] = LAYOUT(
+        _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_VOLD, KC_VOLU, KC_MUTE,
         _______, _______, KC_UP,   _______, _______, _______, _______, _______, KC_INS,  _______, _______, _______, KC_PAUS, _______,
         _______, KC_LEFT, KC_DOWN, KC_RGHT, _______, _______, _______, _______, _______, _______, KC_HOME, KC_PGUP,          EEP_RST,
-        _______,          _______, _______, _______, _______, LK_DFU,  NK_TOGG, _______, _______, KC_END,  KC_PGDN, KC_VOLU, KC_MUTE,
-        _______, _______, _______,                            _______,                   _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
+        _______,          _______, _______, _______, _______, LK_DFU,  NK_TOGG, _______, _______, KC_END,  KC_PGDN, _______, KC_MUTE,
+        _______, _______, _______,                            _______,                   _______, _______, _______, _______, _______
 
     ),
     /*
@@ -59,10 +59,10 @@ void rgb_matrix_indicators_user(void)
 
     // Led colors depending on current layer.
     switch (biton32(layer_state)) {
-        case _BL:
-            rgb_matrix_set_color(13, RGB_GREEN);
+        case _BASE:
+            rgb_matrix_set_color(13, RGB_BLUE);
             break;
-        case _FL:
+        case _FN1:
             rgb_matrix_set_color(13, RGB_RED);
             rgb_matrix_set_color(48, RGB_RED); // Highlight key to go to DFU mode.
             break;
