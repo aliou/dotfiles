2b13ec07534cac5803fbbc8905f17ba1f71a5db3
Aliou Diallo <code@aliou.me>
2022-12-08 10:03:29 +0100

    [code/javascript] Configure nvm when installed through homebrew

--- code/javascript/config.sh
+++ code/javascript/config.sh
@@ -1,12 +1,19 @@
 #!/usr/bin/env bash
+# shellcheck disable=1090
 
 # Configure nvm
 if [[ -d "$HOME/.nvm" ]]; then
   export NVM_DIR="$HOME/.nvm"
 
   # Load nvm.
+  # 1. When nvm is installed manually:
   [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
+  # 2. When nvm is installed through homebrew:
+  [ -s "/usr/local/opt/nvm/nvm.sh" ] && source "/usr/local/opt/nvm/nvm.sh"
 
   # Loading Bash completion.
-  [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+  # 1. When nvm is installed manually:
+  [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
+  # 2. When nvm is installed through homebrew:
+  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
 fi
