952585a5d7d6f4362a330a6b0f59daca3b1841f6
Aliou Diallo <code@aliou.me>
2022-12-17 00:36:43 +0100

    brew: Check installed dependents.

--- brew/config.sh
+++ brew/config.sh
@@ -2,6 +2,3 @@
 
 # Update Homebrew twice a week.
 export HOMEBREW_AUTO_UPDATE_SECS=302400
-
-# Avoid updating outdated dependents when running `brew upgrade`.
-export HOMEBREW_NO_INSTALLED_DEPENDENTS_CHECK=true
