02f96c5d99810cd6e88126e8d62f05bc8905843a
Aliou Diallo <code@aliou.me>
2022-12-16 13:42:50 +0100

    code/javascript: Load nvm from home dir

--- code/javascript/config.sh
+++ code/javascript/config.sh
@@ -1,19 +1,13 @@
 #!/usr/bin/env bash
-# shellcheck disable=1090
+# shellcheck disable=1091
 
 # Configure nvm
 if [[ -d "$HOME/.nvm" ]]; then
   export NVM_DIR="$HOME/.nvm"
 
   # Load nvm.
-  # 1. When nvm is installed manually:
   [ -s "$NVM_DIR/nvm.sh" ] && source "$NVM_DIR/nvm.sh"
-  # 2. When nvm is installed through homebrew:
-  [ -s "/usr/local/opt/nvm/nvm.sh" ] && source "/usr/local/opt/nvm/nvm.sh"
 
   # Loading Bash completion.
-  # 1. When nvm is installed manually:
   [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
-  # 2. When nvm is installed through homebrew:
-  [ -s "/usr/local/opt/nvm/etc/bash_completion.d/nvm" ] && source "/usr/local/opt/nvm/etc/bash_completion.d/nvm"
 fi
