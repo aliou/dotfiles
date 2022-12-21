f0316ee0b7ad44072b742ea8dd9f42abb17387f2
Aliou Diallo <code@aliou.me>
2022-12-21 18:52:09 +0100

    code/docker: Set DOCKER_HOST when colima is running.

--- code/docker/config.sh
+++ code/docker/config.sh
@@ -1,3 +1,13 @@
 #!/usr/bin/env bash
 
 export WHALEBREW_INSTALL_PATH="$HOME/code/bin"
+
+# Setup DOCKER_HOST env variables when colima is running.
+if [[ -d "$HOME~/.colima" ]]; then
+  current_context=$(docker context inspect | jq -r .[0].Name)
+
+  if [[ $current_context == "colima" ]]; then
+    docker_host=$(docker context inspect | jq -r .[0].Endpoints.docker.Host)
+    export DOCKER_HOST=$docker_host
+  fi
+fi
