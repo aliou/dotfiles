8e5fa1997a41bc1d86b87b21c7f4e31dffd1da51
Aliou Diallo <code@aliou.me>
2022-12-21 18:51:55 +0100

    code/docker: Remove unused docker-compose aliases.

--- code/docker/config.sh
+++ code/docker/config.sh
@@ -1,12 +1,3 @@
 #!/usr/bin/env bash
 
-if [[ -n `which docker-compose` ]]; then
-  alias dcp='docker-compose'
-  alias dcpl='docker-compose logs -f'
-  alias dcpu='docker-compose up'
-  alias dcpr='docker-compose run --rm'
-
-  alias dcpul="docker-compose-up-and-logs"
-fi
-
 export WHALEBREW_INSTALL_PATH="$HOME/code/bin"
