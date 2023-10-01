cfa884aebc1bc1e29741227c8055f91a19e9e7c2
Aliou Diallo <code@aliou.me>
2023-10-01 19:29:39 +0200

    code/js: setup tab completion for pnpm

--- code/javascript/config.sh
+++ code/javascript/config.sh
@@ -11,3 +11,7 @@ if [[ -d "$HOME/.nvm" ]]; then
   # Loading Bash completion.
   [ -s "$NVM_DIR/bash_completion" ] && source "$NVM_DIR/bash_completion"
 fi
+
+# Configure tab completion for pnpm.
+# shellcheck disable=2015
+[ -f ~/.config/tabtab/bash/__tabtab.bash ] && . "$HOME/.config/tabtab/bash/__tabtab.bash" || true
